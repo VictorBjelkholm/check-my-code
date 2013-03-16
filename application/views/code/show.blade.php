dad3de030d75c4c627bda252eac37e98a0d70432
VictorBjelkholm <victorbjelkholm@gmail.com>
2013-03-16 08:03:19 +0100

    fixed errors

--- application/views/code/show.blade.php
+++ application/views/code/show.blade.php
@@ -6,12 +6,5 @@
 	<p>Syntax: {{ $code->syntax}} </p>
 	<p><pre class="prettyprint linenums"><code class="language-{{ $code->syntax }}">{{ $code->content }}</code></pre></p>
 	<p>Created at {{ $code->created_at}} by <a href="{{ URL::to_route('show_user', $code->users->username) }}">{{ $code->users->name }}</a></p>
-	@foreach($code->comments as $comment)
-		<div class="comment">
-		<h4>{{ $comment->created_at }}</h4>
-		<p>{{ $comment->body }}<p>
-		<p>{{ $comment->users->name }}</p>
-		</div>
-	@endforeach
 	@include('code.controls')
 @endsection
